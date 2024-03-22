db8751eb123009f89a10f4887a76b88b7b841af8
Oguzhan Yusuf Dogan <oguzhanyusuf.dogan@gmail.com>
2024-03-22 16:04:52 +0300

    homepage, sidebar, theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "./assets/compiled/css/app.css"
+import "./assets/compiled/css/app-dark.css"
+import "./assets/compiled/js/app.js"
+import "./assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"
+//import "./assets/extensions/apexcharts/apexcharts.min.js"
+//import "./assets/static/js/pages/dashboard.js"
+import Home from './components/Home/Home'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Sidebar />
+      <Home />
     </>
   )
 }
