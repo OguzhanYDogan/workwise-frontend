bd599e8c3466ed1891199a8d3f86683bac91a961
Oguzhan Yusuf Dogan <oguzhanyusuf.dogan@gmail.com>
2024-04-05 14:55:53 +0300

    Template component added for routing main pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,62 +7,22 @@ import "./assets/compiled/css/auth.css"
 import "./assets/compiled/css/app-dark.css"
 import "./assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"
 import 'react-toastify/dist/ReactToastify.css';
-import Sidebar from './components/Sidebar/Sidebar'
-import ProfileUpdate from './components/Profile/ProfileUpdate.jsx'
-import ProfileDetail from './components/Profile/ProfileDetail.jsx'
-import Profile from './components/Profile/Profile.jsx'
-import SidebarToggler from './components/Sidebar/SidebarToggler.jsx'
-import CompanyList from './components/Company/CompanyList.jsx'
-import CompanyAdd from './components/Company/CompanyAdd.jsx'
-import NotFound from "./components/Error/NotFound.jsx";
-import CompanyDetail from "./components/Company/CompanyDetail.jsx";
-import CompanyManagerAdd from "./components/CompanyManager/CompanyManagerAdd.jsx";
-import CompanyManagerList from "./components/CompanyManager/CompanyManagerList.jsx";
-import { ToastContainer } from 'react-toastify';
 import Login from "./components/Authentication/Login.jsx";
 import ForgotPassword from "./components/Authentication/ForgotPassword.jsx";
 import ResetPassword from "./components/Authentication/ResetPassword.jsx";
-import PersonelAdd from "./components/Personel/PersonelAdd.jsx";
+import Template from "./components/Template.jsx";
 
 function App() {
-  const [isActive, setIsActive] = useState(false);
   const [theme, setTheme] = useState("light");
 
   return (
     <BrowserRouter>
-      <Sidebar isActive={isActive} setIsActive={setIsActive} theme={theme} setTheme={setTheme} />
-      <div id="main">
-        <SidebarToggler isActive={isActive} setIsActive={setIsActive} />
-        <div id="app">
-          <ToastContainer theme={theme} hideProgressBar={false} />
-          <Routes>
-            <Route path="/" element={<Profile />} />
-            <Route path="/update" element={<ProfileUpdate />} />
-            <Route path="/detail" element={<ProfileDetail />} />
-            <Route path="/company-list" element={<CompanyList />} />
-            <Route path="/company-detail/:id" element={<CompanyDetail />} />
-            <Route path="/company-add" element={<CompanyAdd />} />
-            <Route path="/company-manager-add" element={<CompanyManagerAdd />} />
-            <Route path="/company-manager-list" element={<CompanyManagerList />} />
-            <Route path="/personel-add" element={<PersonelAdd />} />
-            <Route path="/login" element={<Login theme={theme} setTheme={setTheme} />} />
-            <Route path="/forgot-password" element={<ForgotPassword theme={theme} setTheme={setTheme} />} />
-            <Route path="/reset-password" element={<ResetPassword theme={theme} setTheme={setTheme} />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-        <footer>
-          <hr />
-          <div class="footer clearfix mb-0 text-muted">
-            <div class="float-start">
-              <p>2024 © WorkWise</p>
-            </div>
-            <div class="float-end">
-              <p>All Rights Reserved.</p>
-            </div>
-          </div>
-        </footer>
-      </div>
+      <Routes>
+        <Route path="/*" element={<Template theme={theme} setTheme={setTheme} />} />
+        <Route path="/login" element={<Login theme={theme} setTheme={setTheme} />} />
+        <Route path="/forgot-password" element={<ForgotPassword theme={theme} setTheme={setTheme} />} />
+        <Route path="/reset-password" element={<ResetPassword theme={theme} setTheme={setTheme} />} />
+      </Routes>
     </BrowserRouter>
   )
 }
---
+++ src/components/Template.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Profile from './Profile/Profile'
+import ProfileUpdate from './Profile/ProfileUpdate'
+import ProfileDetail from './Profile/ProfileDetail'
+import CompanyList from './Company/CompanyList'
+import CompanyDetail from './Company/CompanyDetail'
+import CompanyAdd from './Company/CompanyAdd'
+import CompanyManagerAdd from './CompanyManager/CompanyManagerAdd'
+import CompanyManagerList from './CompanyManager/CompanyManagerList'
+import PersonelAdd from './Personel/PersonelAdd'
+import Sidebar from './Sidebar/Sidebar'
+import SidebarToggler from './Sidebar/SidebarToggler'
+import { ToastContainer } from 'react-toastify';
+import NotFound from './Error/NotFound'
+
+function Template({ theme, setTheme }) {
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+        <>
+            <Sidebar isActive={isActive} setIsActive={setIsActive} theme={theme} setTheme={setTheme} />
+            <div id="main">
+                <SidebarToggler isActive={isActive} setIsActive={setIsActive} />
+                <div id="app">
+                    <ToastContainer theme={theme} hideProgressBar={false} />
+                    <Routes>
+                        <Route path="/" element={<Profile />} />
+                        <Route path="/update" element={<ProfileUpdate />} />
+                        <Route path="/detail" element={<ProfileDetail />} />
+                        <Route path="/company-list" element={<CompanyList />} />
+                        <Route path="/company-detail/:id" element={<CompanyDetail />} />
+                        <Route path="/company-add" element={<CompanyAdd />} />
+                        <Route path="/company-manager-add" element={<CompanyManagerAdd />} />
+                        <Route path="/company-manager-list" element={<CompanyManagerList />} />
+                        <Route path="/personel-add" element={<PersonelAdd />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </div>
+                <footer>
+                    <hr />
+                    <div class="footer clearfix mb-0 text-muted">
+                        <div class="float-start">
+                            <p>2024 © WorkWise</p>
+                        </div>
+                        <div class="float-end">
+                            <p>All Rights Reserved.</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </>
+    )
+}
+
+export default Template
